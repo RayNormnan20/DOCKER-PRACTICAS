a1902b843ab0592c5fbfe457bd0c65e92892c9f7
Jose Pintado <josepintadoyamo@gmail.com>
2024-06-03 17:24:49 -0500

    Docker 1

---
+++ dockerized.postgresql/src/main/java/com/pee/dockerized/postgresql/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.pee.dockerized.postgresql.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
