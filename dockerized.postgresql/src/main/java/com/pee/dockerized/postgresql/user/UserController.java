ce7052067768644e7ab5a0afdd47084dac5eca3b
Jose Pintado <josepintadoyamo@gmail.com>
2024-06-14 15:24:19 -0500

    Docker 2

--- dockerized.postgresql/src/main/java/com/pee/dockerized/postgresql/user/UserController.java
+++ dockerized.postgresql/src/main/java/com/pee/dockerized/postgresql/user/UserController.java
@@ -1,36 +1,57 @@
 package com.pee.dockerized.postgresql.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin  // Intercambio de datos con otros origenes
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@GetMapping
-	public List<User>getAllUsers(){
-		return userRepository.findAll();
-	}
-	
-	@GetMapping("/{id}")
-	public User getUserById(@PathVariable Long id) {
-		return userRepository.findById(id).get();
-	}
-	
-	@PostMapping
-	public User createUser(@RequestBody User user) {
-		return userRepository.save(user);
-	}
-	
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setName(userDetails.getName());
+            return userRepository.save(user);
+        } else {
+            return null; // Handle this case as needed (e.g., throw exception or return a response entity with 404 status)
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
 }
